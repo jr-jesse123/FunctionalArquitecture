84c4e450157a931177ff57097c74b9c007d3265d
jr-jesse123 <jesse@quatrodconsultoria.com.br>
2021-10-12 17:47:35 -0300

    primeiro endpoint criado e exposto

--- CafeApp.Web/Program.fs
+++ CafeApp.Web/Program.fs
@@ -1,13 +1,48 @@
 ﻿// Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
 
 open System
+open Suave.Web
+open Suave.Successful
+open Suave.RequestErrors
+open Suave.Operators
+open Suave.Filters
+open CommandApi
+open InMemory
+open System.Text
+open Suave.Headers.Fields
+open Suave
 
-// Define a function to construct a message to print
-let from whom =
-    sprintf "from %s" whom
+let commandApihandler eventStore (context: HttpContext) = async{
+   let payload = Encoding.UTF8.GetString context.request.rawForm
+   let! response = 
+      handleCommandRequest inMemoryQueries eventStore payload
+   match response with
+   | Ok (state,events) -> 
+      return! OK (sprintf "%A" state) context
+
+   |Error err -> 
+      return! BAD_REQUEST (err.Message) context
+
+}
+
+
+let  commandApi eventStore = 
+   path "/command"
+      >=> Filters.POST  
+      >=> commandApihandler eventStore
+
 
 [<EntryPoint>]
 let main argv =
-    let message = from "F#" // Call the function
-    printfn "Hello world %s" message
-    0 // return an integer exit code
+   let app = 
+      let eventStore = inMemoryEventStore ()
+      choose [
+         commandApi eventStore
+      ]
+
+   let cfg = 
+      {defaultConfig with bindings=[HttpBinding.createSimple HTTP "0.0.0.0" 8083]}
+
+   startWebServer cfg app
+
+   0 
